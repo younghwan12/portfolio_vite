a6372eb8bf8fa6b54492328be80a99cef09d3b8e
younghwan12 <dowk3432@gmail.com>
2022-12-13 15:19:10 +0900

    2022.12.13

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                 <div>Github : <a href="https://github.com/younghwan12" target="_blank">github.com/younghwan12</a></div>
                 <div>Blog : <a href="https://younghwan112.tistory.com/" target="_blank">younghwan112.tistory.com</a></div>
                 <div className="decor profile2">
-                    <img src="../src/assets/img/profile2.png" alt="profile02" />
+                    <img src="images/profile2.png" alt="profile02" />
                 </div>
             </div>
         </div>            
